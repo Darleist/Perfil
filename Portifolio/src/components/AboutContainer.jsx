1cfab5cb1059e6034d61581409fa5437bee7a7c2
Darlei Strege <86538945+Darleist@users.noreply.github.com>
2022-08-24 15:46:45 -0400

    Finally

--- Portifolio/src/components/AboutContainer.jsx
+++ Portifolio/src/components/AboutContainer.jsx
@@ -4,8 +4,9 @@ const AboutContainer = () => {
   return (
     <section className="about-container">
       <h2>Sobre</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure adipisci repudiandae molestias consequuntur quos earum qui eos facere consectetur nam, tempora consequatur placeat praesentium aliquam at enim suscipit vel eligendi.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, eligendi officia dolor illum iste perspiciatis nihil! Modi aperiam ea consectetur, nobis voluptatum provident autem quos voluptate, odio, enim itaque rem.</p>
+      <p>Meu nome é Darlei Strege Trechel, nascido no Paraguay, e tendo morado lá por 26 anos. Atualmente estou morando la linda cidade de Foz do Iguaçu. Sou felizmente casado com Paula Gabriela Mattei Strege com tem tenho dois filhos: Josias Mattei Strege e Mikael Mattei Strege.</p>
+      <p>Tenho baicharel el Teología pelo CEMTA, tendo exercido a profissão até os 27 anos. Atualmente venho me capacitando na área da tecnología, especialmente no Front-End com as tecnologias citadas logo abaixo. Meu objetivo é me tornar um programado profesional, para tal meta, estudo regularmente os conteúdos onlines disponíveis.</p>
+      <p>Sou fluente em Espanhol e Portugues</p>
     </section>
   )
 }
