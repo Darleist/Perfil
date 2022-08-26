b26823d5f821620cdf489b3eaa4cafaf195518b8
Darlei Strege <86538945+Darleist@users.noreply.github.com>
2022-08-26 19:06:07 -0400

    Pronto, agora refazer pra praticar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MainContent from "./components/MainContent"
+import Sidebar from "./components/Sidebar"
+
+import './styles/components/app.sass'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Darlei Strege</h1>
+      <Sidebar/>
+      <MainContent/>
+    </div>
+  )
+}
+
+export default App
